3c60f257f3375332658235134c131031f106b625
Ever-PR <ever.g.pr2019@gmail.com>
2024-07-15 16:30:10 -0500

    cambio de config

--- classes/config.php
+++ classes/config.php
@@ -1,7 +1,7 @@
 <?php /** @package EBC * @author Elmer Blas * @e-mail elmer.blas@gmail.com and kimer_12@hotmail.com * @copyright Elmer Blas */
 $CONFIG['dbtype'] = 'mysql';
 $CONFIG['dbuser'] = 'root';
-$CONFIG['dbpass'] = 'cmspprivera';
+$CONFIG['dbpass'] = '';
 $CONFIG['dbname'] = 'ventas';
 $CONFIG['dbhost'] = 'localhost';
 $CONFIG['dbport'] = '3306';
